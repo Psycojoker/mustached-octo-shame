ca9ac40bdcaf37b21e945a5a390335791bc10beb
Laurent Peuch <cortex@worlddomination.be>
2014-03-13 18:37:03 +0100

    [enh] display comments

--- mur.py
+++ mur.py
@@ -6,9 +6,20 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     #posts = get_posts()
-    posts = [{"title": "pouet", "link": "http://tamere.com", "author": "haxe", "date": datetime.now()}]
+    posts = [{"id": 1, "title": "pouet", "link": "http://tamere.com", "author": "haxe", "date": datetime.now()}]
 
     return render_template("home.html", posts=posts)
+
+
+@app.route("/<id>")
+def post(id):
+    #post = get_post(id)
+    post = {"title": "pouet", "link": "http://tamere.com", "author": "haxe", "date": datetime.now(), "text": "this is the <i>text</i>",
+            "comments": [{
+                "author": "Bram",
+                "text": "youplaboum",
+            }]}
+    return render_template("post.html", post=post)
 
 
 if __name__ == "__main__":
