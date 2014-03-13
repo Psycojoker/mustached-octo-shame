6369713ee73bc4936c8ffdcb5fdc2d0af7677be4
Charles Hubain <haxelion@gmail.com>
2014-03-13 18:36:58 +0100

    Read only ORM.

--- stonecarver.py
+++ stonecarver.py
@@ -1,4 +1,7 @@
 from peewee import *
+import datetime
+
+db = SqliteDatabase('stones.db')
 
 class Post(Model):
     title = CharField()
@@ -6,8 +9,23 @@ class Post(Model):
     link = CharField()
     text = TextField()
     date = DateField()
+
+    class Meta:
+        database = db
  
 class Comment(Model):
     author = CharField()
     text = TextField()
     date = DateField()
+    
+    class Meta:
+        database = db
+ 
+def init():
+    db.connect()
+    Post.create_table(True)
+    Comment.create_table(True)
+    Post.create(title='Hello World', author='Bram & Haxe', link = '/', text='Welcome to Mustached Octo Shame !.', date=datetime.datetime.now()).save()
+
+def get_posts():
+    return list(Post.select().order_by(Post.date.desc()))
