4a07a32bb67db350c9417c134bd0585df443d853
Charles Hubain <haxelion@gmail.com>
2014-03-21 00:23:04 +0100

    New post can now be submited.

--- mur.py
+++ mur.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from flask import Flask, render_template
-from stonecarver import init, get_posts, get_post
+from flask import Flask, render_template, request, redirect
+from stonecarver import init, get_posts, get_post, store_post
 app = Flask(__name__)
 
 
@@ -9,8 +9,15 @@ def home():
     posts = get_posts()
     return render_template("home.html", posts=posts)
 
+@app.route("/new", methods=['GET', 'POST'])
+def new():
+    if request.method == 'GET':
+        return render_template("new.html")
+    else:
+        store_post(request.form["title"], request.form["author"], request.form["link"], request.form["text"], datetime.now())
+        return redirect("/")
 
-@app.route("/<id>")
+@app.route("/<int:id>")
 def post(id):
     post = get_post(id)
     return render_template("post.html", post=post)
